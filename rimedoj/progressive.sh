3243e84933a7e14e1935f3b75ce12487e3df8f2c
Martin Bodin <martin.bodin@ens-lyon.org>
2020-06-23 17:58:37 +0100

    Adding sources of applications.

--- rimedoj/progressive.sh
+++ rimedoj/progressive.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Make all jpg images progressive.
+
 for i in *.jpg; do
  echo $i
  jpegtran -copy none -optimize -outfile temp_optimize.jpg $i
  jpegtran -copy none -progressive -outfile $i temp_optimize.jpg
  rm temp_optimize.jpg
 done
+
